388f140e6eaa0f60f71d1d55338c26aea4005742
Rakibul Anam Rohid <rakibul.anam@brainstation-23.com>
2024-04-25 13:38:40 +0600

    solved problem 2

---
+++ two_sum.java
@@ -0,0 +1,13 @@
+public class two_sum {
+
+    public static void main(String[] args) {
+        AddTwoNumbers(10, 20);
+    }
+
+    public static void AddTwoNumbers(int firstNumber, int secondNumber){
+        int sumOfNumbers = firstNumber + secondNumber;
+
+        System.out.println(sumOfNumbers);
+    }
+
+}
