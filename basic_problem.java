d13e9f674121da67fb8a8c232ef34677443250c1
Rakibul Anam Rohid <rakibul.anam@brainstation-23.com>
2024-04-25 15:55:57 +0600

    Made the code more readable

--- basic_problem.java
+++ basic_problem.java
@@ -2,12 +2,8 @@ public class basic_problem {
 
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        System.out.println("Hello World");
-=======
         System.out.println("Hello Github World");
         System.out.println("I am here to stash");
->>>>>>> Stashed changes
     }
     
 }
