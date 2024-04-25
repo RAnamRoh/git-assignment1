339d6d5907fe911a0ee6fee8889a684c38852877
Rakibul Anam Rohid <rakibul.anam@brainstation-23.com>
2024-04-25 14:04:56 +0600

    Extended the soultion to problem 2

--- two_sum.java
+++ two_sum.java
@@ -2,10 +2,17 @@ public class two_sum {
 
     public static void main(String[] args) {
         AddTwoNumbers(10, 20);
+        SubtractTwoNumbers(50, 30);
     }
 
     public static void AddTwoNumbers(int firstNumber, int secondNumber){
         int sumOfNumbers = firstNumber + secondNumber;
+
+        System.out.println(sumOfNumbers);
+    }
+
+    public static void SubtractTwoNumbers(int firstNumber, int secondNumber){
+        int sumOfNumbers = firstNumber - secondNumber;
 
         System.out.println(sumOfNumbers);
     }
